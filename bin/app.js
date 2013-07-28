40f51bee24e1e7f54b2c464872d188d009aea3ce
kyung yeol kim <chitacan@gmail.com>
2013-07-28 13:28:40 +0900

    process argv

--- bin/app.js
+++ bin/app.js
@@ -1,5 +1,35 @@
 #!/usr/bin/env node
 
-// show help
+var opt  = {};
+var args = process.argv.slice(2);
 
-require('./../lib/index.js').init();
+if (!args.length) {
+	console.log('(no user specified. `starred -h` for usage');
+	process.exit(1);
+}
+
+while(args.length > 0) {
+	var arg = args.shift();
+	switch(arg) {
+		case '-h':
+		case '--help':
+			printHelp();
+			break;
+		default:
+			opt.id = arg;
+			break;
+	}
+}
+
+function printHelp() {
+	console.log('');
+	console.log('   Usage: starred [userid]');
+	console.log('');
+	console.log('   Options:');
+	console.log('');
+	console.log('     -h, --help    print this help');
+	console.log('');
+	process.exit(0);
+}
+
+// require('./../lib/index.js').init(opt);
